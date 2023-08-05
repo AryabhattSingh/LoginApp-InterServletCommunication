db67dcbb9494d27b37b9c0b288f8596acc37b0f0
Aryabhatt <bhattsahab314@gmail.com>
2023-08-06 05:22:05 +0530

    Added comments in HomeServlet

--- src/main/java/interServletCommunication/HomeServlet.java
+++ src/main/java/interServletCommunication/HomeServlet.java
@@ -24,14 +24,20 @@ public class HomeServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
+		// Getting a PrintWriter object in order to write back to the browser
 		PrintWriter out = response.getWriter();
+
+		// Setting the content type / MIME type, so that the browser understands the
+		// things written to it
 		response.setContentType("text/html");
 
+		// Writing back to the browser
 		out.print("<html>" + "<head>" + "<title>Login Successful</title>" + "</head>"
 				+ "<body style='overflow-y: hidden'>"
 				+ "<h2 style='border: 2px solid green; height: 97vh; display: flex; flex-direction: column; "
-				+ "justify-content: center; align-items: center;}'>" + request.getAttribute("message") + "</h2>"
-				+ "</body>" + "</html");
+				+ "justify-content: center; align-items: center;}'>"
+				// Getting the message which was set in LoginServlet
+				+ request.getAttribute("message") + "</h2>" + "</body>" + "</html");
 
 		System.out.println("doPost() used");
 
