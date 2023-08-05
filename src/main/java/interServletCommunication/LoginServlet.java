aeb1a8a95117c6d1b9ee90ea5609e5cc4322da66
Aryabhatt <bhattsahab314@gmail.com>
2023-08-06 05:21:24 +0530

    Added comments in LoginServlet

--- src/main/java/interServletCommunication/LoginServlet.java
+++ src/main/java/interServletCommunication/LoginServlet.java
@@ -26,10 +26,17 @@ public class LoginServlet extends HttpServlet {
 	@Override
 	public void init() {
 		try {
+
+			// Created an object of ServletContext to access context params set by web.xml
 			ServletContext context = getServletContext();
+
+			// Loading the MySQL Driver
 			Class.forName(context.getInitParameter("SQLDriver"));
+
+			// Creating a connection to the database.
 			connection = DriverManager.getConnection(context.getInitParameter("dbUrl"),
 					context.getInitParameter("dbUser"), context.getInitParameter("dbPassword"));
+
 		} catch (ClassNotFoundException | SQLException e) {
 			e.printStackTrace();
 		}
@@ -44,22 +51,45 @@ public class LoginServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
+		// Retrieving data submitted by the login form for processing
 		String username = request.getParameter("username");
 		String password = request.getParameter("password");
 
 		try {
+
+			// Creating a Statement object in order to execute a sql query
 			Statement statement = connection.createStatement();
+
+			// Creating a ResultSet object to store records returned after statement object
+			// executes sql query
 			ResultSet resultSet = statement.executeQuery(
 					"select firstName from user where email = '" + username + "' and password = '" + password + "'");
 
+			// Creating a RequestDispatcher object to forward request to HomeServlet
 			RequestDispatcher requestDispatcher = request.getRequestDispatcher("homeServlet");
 
-			if (resultSet.next()) {
+			if (resultSet.next()) { // next() checks if the resultSet has records and moves the cursor pointer to
+									// the row
+
+				// Retrieving firstName of user from the resultSet as returned by database;
+				// In resultSet the indexing starts from 1'
 				String firstName = resultSet.getString(1);
+
+				// Setting the message and it's value. setAttribute() will store this in the
+				// request.
 				request.setAttribute("message", firstName + ", welcome to the world of Inter Servlet Communication");
+
+				// The request is forwarded to another servlet
 				requestDispatcher.forward(request, response);
+
 			} else {
+
+				// In case of login failure, below things will happen
+
+				// Setting the requestDispatcher object to new value
 				requestDispatcher = request.getRequestDispatcher("login.html");
+
+				// Includes the content of login.html in the response
 				requestDispatcher.include(request, response);
 			}
 
@@ -72,6 +102,7 @@ public class LoginServlet extends HttpServlet {
 	@Override
 	public void destroy() {
 		try {
+			// closing the database connection of this servlet
 			connection.close();
 		} catch (SQLException e) {
 			e.printStackTrace();
